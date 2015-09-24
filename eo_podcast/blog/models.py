7f2f1056581934fbab388fb25ad463bd7979efb7
DJO3 <dev@pytools.org>
2015-09-23 22:05:47 -0400

    added models for Entry and Tags

--- eo_podcast/blog/models.py
+++ eo_podcast/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+# Tags to accompany each Entry
+class Tags(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+# Blog Posts
+class Entry(models.Model):
+    title = models.CharField(max_length=140, unique=True)
+    body = models.TextField()
+    url = models.SlugField(max_length=200, unique=True)
+    published = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tags, related_name='entries')
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
